43e316c4a7f8ca18e71fd6a324587a7a71d20dc8
JH <961519077@qq.com>
2019-02-25 11:30:28 +0800

    第二阶段项目第一次创建

---
+++ Electronic_City/Electronic_City/src/com/ElectronicCity/dao/BaseDao.java
@@ -0,0 +1,124 @@
+package com.ElectronicCity.dao;
+
+
+import com.ElectronicCity.util.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BaseDao {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    private ResultSet rs = null;
+
+    /**
+     * 创建连接
+     *
+     * @return
+     */
+    public Connection getConn() {
+        String driver = Configuration.getConfig().getkey("driver");
+        String url = Configuration.getConfig().getkey("url");
+        String user = Configuration.getConfig().getkey("username");
+        String password = Configuration.getConfig().getkey("password");
+        try {
+            // 1、加载驱动
+            Class.forName(driver);
+            // 2、建立连接
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+      /*  Context initcontext;
+        try {
+            initcontext = new InitialContext();
+            Context context = (Context) initcontext.lookup("java:comp/env");
+            DataSource datasource = (DataSource) context.lookup("jdbc/news");
+            conn = datasource.getConnection();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;*/
+    }
+
+    /**
+     * 关闭资源
+     *
+     * @param conn
+     * @param ps
+     * @param rs
+     */
+    public void closeAll(Connection conn, PreparedStatement ps, ResultSet rs) {
+        try {
+            if (rs != null)
+                rs.close();
+            if (ps != null)
+                ps.close();
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 针对查询
+     *
+     * @param sql
+     * @param obj
+     * @return
+     */
+    public ResultSet executeQuery(String sql, Object[] obj) {
+        // 创建连接
+        conn = this.getConn();
+        try {
+            // 欲执行
+            ps = conn.prepareStatement(sql);
+            if (obj != null) {// 判断参数是否为null，如果不为null，进行循环传参
+                for (int i = 0; i < obj.length; i++) {
+                    ps.setObject((i + 1), obj[i]);
+                }
+            }
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    /**
+     * 针对增删改
+     *
+     * @param sql
+     * @param obj
+     * @return
+     */
+    public int executeUpdate(String sql, Object[] obj) {
+        int result = 0;
+        // 创建连接
+        conn = this.getConn();
+        try {
+            // 欲执行
+            ps = conn.prepareStatement(sql);
+            if (obj != null) {// 判断参数是否为null，如果不为null，进行循环传参
+                for (int i = 0; i < obj.length; i++) {
+                    ps.setObject((i + 1), obj[i]);
+                }
+            }
+            result = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            this.closeAll(conn, ps, null);
+        }
+        return result;
+    }
+}
